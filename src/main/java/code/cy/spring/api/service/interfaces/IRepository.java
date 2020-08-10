01eff7b42b2cc8ca98304179c3bae242ad0eada4
Camilo Barbosa <cab331@hotmail.com>
2020-08-10 13:02:59 -0500

    1.0.1

--- src/main/java/code/cy/spring/api/service/interfaces/IRepository.java
+++ src/main/java/code/cy/spring/api/service/interfaces/IRepository.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IRepository<T, ID extends Serializable> extends CrudRepository<T, ID> {
     public Optional<T> findById(ID id);
 
